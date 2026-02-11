edc9d6bc315b871472e6f668bc27f91c709222fc
Tony <ttranusanvy@gmail.com>
2026-02-11 08:46:42 -0800

    fix(app): multi-select delete, Select All, fleet AD discovery, package version fix
    - Import library ListView supports Shift/Ctrl multi-select (SelectionMode=Extended)
    - Delete Selected now deletes all highlighted packs, not just the first
    - Select All button added to import library actions
    - System.DirectoryServices bumped to 9.0.0 (fixes NU1603 warning)
    - Fleet AD discovery queries DirectorySearcher for computer objects

--- src/STIGForge.App/Views/ImportView.xaml.cs
+++ src/STIGForge.App/Views/ImportView.xaml.cs
@@ -1,6 +1,24 @@
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using STIGForge.App;
 namespace STIGForge.App.Views;
 public partial class ImportView : UserControl
 {
     public ImportView() { InitializeComponent(); }
+
+    private void ContentLibraryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (DataContext is MainViewModel vm)
+        {
+            vm.SelectedLibraryItems.Clear();
+            foreach (var item in ContentLibraryList.SelectedItems.OfType<MainViewModel.ImportedLibraryItem>())
+                vm.SelectedLibraryItems.Add(item);
+        }
+    }
+
+    private void SelectAllLibraryItems_Click(object sender, RoutedEventArgs e)
+    {
+        ContentLibraryList.SelectAll();
+    }
 }
