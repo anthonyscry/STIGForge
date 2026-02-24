12cf6add8ab5937d4c938f06e28ff9c516d415cc
Anthony Tran <anthonyscry@users.noreply.github.com>
2026-02-23 17:40:15 -0800

    test(app): validate mission path sync when datacontext set pre-load

--- tests/STIGForge.UnitTests/Views/ImportViewLayoutContractTests.cs
+++ tests/STIGForge.UnitTests/Views/ImportViewLayoutContractTests.cs
@@ -1,6 +1,8 @@
 using System.Xml.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using STIGForge.App;
 using STIGForge.App.Views;
 
 namespace STIGForge.UnitTests.Views;
@@ -162,232 +164,25 @@ public sealed class ImportViewLayoutContractTests
   }
 
   [Fact]
-  public void ImportView_OnDataContextChanged_BindsDirectlyWithoutLoadGate()
+  public void ImportView_MissionPathSyncs_WhenDataContextSetBeforeLoaded()
   {
-    var type = typeof(ImportView);
-    var onDataContextChanged = type.GetMethod("OnDataContextChanged", BindingFlags.NonPublic | BindingFlags.Instance);
-    Assert.NotNull(onDataContextChanged);
+    var view = new ImportView();
+    var vm = (MainViewModel)RuntimeHelpers.GetUninitializedObject(typeof(MainViewModel));
 
-    var bindToViewModel = type.GetMethod("BindToViewModel", BindingFlags.NonPublic | BindingFlags.Instance);
-    Assert.NotNull(bindToViewModel);
+    var onLoaded = typeof(ImportView).GetMethod("OnLoaded", BindingFlags.NonPublic | BindingFlags.Instance);
+    var onUnloaded = typeof(ImportView).GetMethod("OnUnloaded", BindingFlags.NonPublic | BindingFlags.Instance);
+    Assert.NotNull(onLoaded);
+    Assert.NotNull(onUnloaded);
 
-    var boundViewModelField = type.GetField("_boundViewModel", BindingFlags.NonPublic | BindingFlags.Instance);
-    Assert.NotNull(boundViewModelField);
+    view.DataContext = vm;
+    onLoaded!.Invoke(view, new object[] { view, new RoutedEventArgs(FrameworkElement.LoadedEvent) });
 
-    var memberTokens = ReadReferencedMemberTokens(onDataContextChanged!);
-    Assert.Contains(bindToViewModel!.MetadataToken, memberTokens);
-    Assert.DoesNotContain(boundViewModelField!.MetadataToken, memberTokens);
-  }
+    const string missionPath = @"C:\temp\mission.json";
+    vm.MissionJsonPath = missionPath;
 
-  private static HashSet<int> ReadReferencedMemberTokens(MethodInfo method)
-  {
-    var body = method.GetMethodBody();
-    Assert.True(body is not null, $"Expected method body for '{method.Name}'.");
+    Assert.Equal(missionPath, view.MissionJsonPath);
 
-    var il = body!.GetILAsByteArray();
-    Assert.True(il is not null && il.Length > 0, $"Expected IL for '{method.Name}'.");
-
-    var tokens = new HashSet<int>();
-    var i = 0;
-    while (i < il!.Length)
-    {
-      var op = il[i++];
-      if (op == 0xFE)
-      {
-        if (i >= il.Length)
-          break;
-
-        var ext = il[i++];
-        if (ext == 0x16 || ext == 0x17 || ext == 0x18 || ext == 0x19 || ext == 0x1A || ext == 0x1B || ext == 0x1C || ext == 0x1D)
-        {
-          if (i + 3 >= il.Length)
-            break;
-
-          tokens.Add(BitConverter.ToInt32(il, i));
-          i += 4;
-        }
-
-        continue;
-      }
-
-      if (op == 0x28 || op == 0x6F || op == 0x73 || op == 0x74 || op == 0x7B || op == 0x7C || op == 0x7D || op == 0x7E || op == 0x7F || op == 0x80)
-      {
-        if (i + 3 >= il.Length)
-          break;
-
-        tokens.Add(BitConverter.ToInt32(il, i));
-        i += 4;
-        continue;
-      }
-
-      i += GetOperandSize(op);
-    }
-
-    return tokens;
-  }
-
-  private static int GetOperandSize(byte op)
-  {
-    return op switch
-    {
-      0x00 => 0,
-      0x01 => 0,
-      0x02 => 0,
-      0x03 => 0,
-      0x04 => 0,
-      0x05 => 0,
-      0x06 => 0,
-      0x07 => 0,
-      0x08 => 0,
-      0x09 => 0,
-      0x0A => 0,
-      0x0B => 0,
-      0x0C => 0,
-      0x0D => 0,
-      0x0E => 1,
-      0x0F => 1,
-      0x10 => 1,
-      0x11 => 1,
-      0x12 => 1,
-      0x13 => 1,
-      0x14 => 0,
-      0x15 => 0,
-      0x16 => 0,
-      0x17 => 0,
-      0x18 => 0,
-      0x19 => 0,
-      0x1A => 0,
-      0x1B => 0,
-      0x1C => 0,
-      0x1D => 0,
-      0x1E => 0,
-      0x1F => 1,
-      0x20 => 4,
-      0x21 => 8,
-      0x22 => 4,
-      0x23 => 8,
-      0x25 => 0,
-      0x26 => 0,
-      0x27 => 0,
-      0x29 => 8,
-      0x2A => 0,
-      0x2B => 1,
-      0x2C => 1,
-      0x2D => 1,
-      0x2E => 1,
-      0x2F => 1,
-      0x30 => 1,
-      0x31 => 1,
-      0x32 => 1,
-      0x33 => 1,
-      0x34 => 1,
-      0x35 => 1,
-      0x36 => 1,
-      0x37 => 1,
-      0x38 => 4,
-      0x39 => 4,
-      0x3A => 4,
-      0x3B => 4,
-      0x3C => 4,
-      0x3D => 4,
-      0x3E => 4,
-      0x3F => 4,
-      0x40 => 4,
-      0x41 => 4,
-      0x42 => 4,
-      0x43 => 4,
-      0x44 => 4,
-      0x45 => 4,
-      0x46 => 4,
-      0x47 => 4,
-      0x48 => 4,
-      0x49 => 4,
-      0x4A => 4,
-      0x4B => 4,
-      0x4C => 4,
-      0x4D => 4,
-      0x4E => 4,
-      0x4F => 4,
-      0x50 => 4,
-      0x51 => 4,
-      0x52 => 4,
-      0x53 => 4,
-      0x54 => 4,
-      0x55 => 4,
-      0x56 => 4,
-      0x57 => 4,
-      0x58 => 0,
-      0x59 => 0,
-      0x5A => 0,
-      0x5B => 0,
-      0x5C => 0,
-      0x5D => 0,
-      0x5E => 0,
-      0x5F => 0,
-      0x60 => 0,
-      0x61 => 0,
-      0x62 => 0,
-      0x63 => 0,
-      0x64 => 0,
-      0x65 => 0,
-      0x66 => 0,
-      0x67 => 0,
-      0x68 => 0,
-      0x69 => 0,
-      0x6A => 0,
-      0x6B => 0,
-      0x6C => 0,
-      0x6D => 0,
-      0x6E => 0,
-      0x70 => 4,
-      0x71 => 4,
-      0x72 => 4,
-      0x75 => 4,
-      0x76 => 4,
-      0x79 => 4,
-      0x7A => 4,
-      0x81 => 4,
-      0x82 => 4,
-      0x83 => 4,
-      0x84 => 2,
-      0x85 => 4,
-      0x86 => 4,
-      0x87 => 4,
-      0x88 => 4,
-      0x89 => 4,
-      0x8A => 4,
-      0x8B => 4,
-      0x8C => 4,
-      0x8D => 4,
-      0x8E => 0,
-      0x8F => 0,
-      0x90 => 0,
-      0x91 => 0,
-      0x92 => 0,
-      0x93 => 0,
-      0x94 => 0,
-      0x95 => 0,
-      0x96 => 0,
-      0x97 => 0,
-      0x98 => 0,
-      0x99 => 0,
-      0x9A => 0,
-      0x9B => 0,
-      0x9C => 0,
-      0x9D => 0,
-      0x9E => 0,
-      0x9F => 0,
-      0xA0 => 0,
-      0xA1 => 0,
-      0xA2 => 0,
-      0xA3 => 0,
-      0xA4 => 0,
-      0xA5 => 0,
-      0xB3 => 1,
-      0xB4 => 4,
-      0xB5 => 0,
-      _ => 0
-    };
+    onUnloaded!.Invoke(view, new object[] { view, new RoutedEventArgs(FrameworkElement.UnloadedEvent) });
   }
 
   private static string LoadImportViewXaml()
