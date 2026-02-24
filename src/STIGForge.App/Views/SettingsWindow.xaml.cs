929c6e2ce978d330b1d044bcdffc549a899089dd
Anthony Tran <anthonyscry@users.noreply.github.com>
2026-02-24 08:55:30 -0800

    feat(views): create SettingsWindow modal dialog

---
+++ src/STIGForge.App/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,28 @@
+using System.Windows;
+
+namespace STIGForge.App.Views;
+
+public partial class SettingsWindow : Window
+{
+  public SettingsWindow()
+  {
+    InitializeComponent();
+  }
+
+  private void Cancel_Click(object sender, RoutedEventArgs e)
+  {
+    DialogResult = false;
+    Close();
+  }
+
+  private void Save_Click(object sender, RoutedEventArgs e)
+  {
+    if (DataContext is WorkflowViewModel vm && vm.SaveSettingsCommand.CanExecute(null))
+    {
+      vm.SaveSettingsCommand.Execute(null);
+    }
+
+    DialogResult = true;
+    Close();
+  }
+}
