e5240ebc2c96721fb6fe15f022def76ced83454d
Anthony Tran <anthonyscry@users.noreply.github.com>
2026-02-23 22:53:06 -0800

    feat(app): add WorkflowWizardView with step content switching

---
+++ src/STIGForge.App/Views/WorkflowWizardView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace STIGForge.App.Views;
+
+public partial class WorkflowWizardView : UserControl
+{
+    public WorkflowWizardView()
+    {
+        InitializeComponent();
+    }
+}
